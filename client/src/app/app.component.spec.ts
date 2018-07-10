7948710c1e6ab2fef55c1f35c71224d5378f5763
leokaos@gmail.com <leokaos@gmail.com>
2018-07-10 14:53:47 +0100

    Ajustes para testes

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,11 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { CompanyService } from './company.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      providers: [CompanyService],
+      imports: [HttpClientTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -22,6 +25,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to web-churros!');
+    expect(compiled.querySelector('h1').textContent).toContain('leo');
   }));
 });
--- client/src/app/company.service.spec.ts
+++ client/src/app/company.service.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { CompanyService } from './company.service';
 
 describe('CompanyService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule],
       providers: [CompanyService]
     });
   });
